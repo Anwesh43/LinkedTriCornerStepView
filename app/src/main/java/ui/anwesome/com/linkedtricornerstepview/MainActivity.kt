14004793c89c7b11543de1290a5ac335032d6356
Anwesh43 <anweshthecool0@gmail.com>
2018-10-18 11:20:38 +0530

    adding fullscreen support

--- app/src/main/java/ui/anwesome/com/linkedtricornerstepview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/linkedtricornerstepview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.linkedtricornerstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.tricornerstepview.TriCornerStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         TriCornerStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
