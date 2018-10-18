a412f084e0967feb9ed297fd71b82006aa0b4c3b
Anwesh43 <anweshthecool0@gmail.com>
2018-10-18 10:46:40 +0530

    importing libraries and defining nodes

--- tricornerstepview/src/main/java/ui/anwesome/com/tricornerstepview/TriCornerStepView.kt
+++ tricornerstepview/src/main/java/ui/anwesome/com/tricornerstepview/TriCornerStepView.kt
@@ -3,3 +3,13 @@ package ui.anwesome.com.tricornerstepview
 /**
  * Created by anweshmishra on 18/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
