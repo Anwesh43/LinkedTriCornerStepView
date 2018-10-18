fb6382292fc898d6da41a6ed76c239ac7cc6afa4
Anwesh43 <anweshthecool0@gmail.com>
2018-10-18 10:48:38 +0530

    adding TriCornerStepView class

--- tricornerstepview/src/main/java/ui/anwesome/com/tricornerstepview/TriCornerStepView.kt
+++ tricornerstepview/src/main/java/ui/anwesome/com/tricornerstepview/TriCornerStepView.kt
@@ -13,3 +13,21 @@ import android.graphics.Color
 import android.graphics.RectF
 
 val nodes : Int = 5
+
+class TriCornerStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
